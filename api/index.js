1fb2c097c9ba47e7b57c9765f9866a107d492944
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-09-04 10:45:23 +0200

    Use dotenv

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const fetch = require("node-fetch");
+global.fetch = fetch;
+
+const Unsplash = require("unsplash-js").default;
+const toJson = require("unsplash-js").toJson;
+
+const unsplash = new Unsplash({
+  accessKey: process.env.ACCESS_KEY,
+});
 
 app.get("/", (req, res) => {
-  res.send("<h2>Just Cat Imgs</h2>");
+  unsplash.photos
+    .getRandomPhoto({ query: "cat" })
+    .then(toJson)
+    .then((json) => {
+      res.send(json);
+    });
 });
 
 const PORT = 3001;
