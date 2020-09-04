aa6c781ac20a53c5d268b6ba2855c8524a33ed09
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-09-04 15:35:57 +0200

    Add crediting for the image

--- src/App.js
+++ src/App.js
@@ -4,41 +4,77 @@ import "./App.css";
 
 const baseUrl = "http://localhost:3001";
 
+const imageObj = {
+  description: "a female adult cat walking in fall leaves.",
+  urls: {
+    raw:
+      "https://images.unsplash.com/photo-1580253784018-8b2c4a9034c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE2Mjk4M30",
+    full:
+      "https://images.unsplash.com/photo-1580253784018-8b2c4a9034c2?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE2Mjk4M30",
+    regular:
+      "https://images.unsplash.com/photo-1580253784018-8b2c4a9034c2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2Mjk4M30",
+    small:
+      "https://images.unsplash.com/photo-1580253784018-8b2c4a9034c2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE2Mjk4M30",
+    thumb:
+      "https://images.unsplash.com/photo-1580253784018-8b2c4a9034c2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE2Mjk4M30",
+  },
+  user: {
+    username: "photographyurway",
+    name: "Diana Orey",
+  },
+};
+
 function App() {
-  const [image, setImage] = useState();
+  const [image, setImage] = useState(imageObj);
 
-  useEffect(() => {
-    const getImage = async () => {
-      if (localStorage.getItem("image")) {
-        setImage(JSON.parse(localStorage.getItem("image")));
-      }
-      try {
-        const response = await axios.get(baseUrl);
-        if (!localStorage.getItem("image")) {
-          setImage(response.data);
-        }
-        localStorage.setItem("image", JSON.stringify(response.data));
-      } catch (error) {
-        console.log("ERROR: ", error);
-      }
-    };
+  // useEffect(() => {
+  //   const getImage = async () => {
+  //     if (localStorage.getItem("image")) {
+  //       setImage(JSON.parse(localStorage.getItem("image")));
+  //     }
+  //     try {
+  //       const response = await axios.get(baseUrl);
+  //       if (!localStorage.getItem("image")) {
+  //         setImage(response.data);
+  //       }
+  //       localStorage.setItem("image", JSON.stringify(response.data));
+  //     } catch (error) {
+  //       console.log("ERROR: ", error);
+  //     }
+  //   };
 
-    if (!image) {
-      getImage();
-    }
-  }, [image]);
+  //   if (!image) {
+  //     getImage();
+  //   }
+  // }, [image]);
 
   return (
     <div className="container">
       {image && <img src={image.urls.regular} alt={image.alt_description} />}
-      {
-        // <img
-        //   src={
-        //     "https://images.unsplash.com/photo-1580253784018-8b2c4a9034c2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2Mjk4M30"
-        //   }
-        //   alt="a female adult cat walking in fall leaves"
-        // />
-      }
+      {/* {
+        <img
+          src={
+            "https://images.unsplash.com/photo-1580253784018-8b2c4a9034c2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2Mjk4M30"
+          }
+          alt="a female adult cat walking in fall leaves"
+        />
+      } */}
+      <p id="credit">
+        Photo by{" "}
+        <a
+          href={
+            "https://unsplash.com/@" +
+            image.user.username +
+            "?utm_source=just_cat_imgs&utm_medium=referral"
+          }
+        >
+          {image.user.name}
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/?utm_source=just_cat_imgs&utm_medium=referral">
+          Unsplash
+        </a>
+      </p>
     </div>
   );
 }
