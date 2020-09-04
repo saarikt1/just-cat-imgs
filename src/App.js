9085f3570852a92d9aad83bab13ab3f42299a241
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-09-04 14:09:42 +0200

    Center image

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
 
 const baseUrl = "http://localhost:3001";
 
 function App() {
   const [image, setImage] = useState();
 
-  useEffect(() => {
-    const getImage = async () => {
-      try {
-        const response = await axios.get(baseUrl);
-        setImage(response.data);
-        console.log("Image set", response.data.alt_description);
-      } catch (error) {
-        console.log("ERROR: ", error);
-      }
-    };
+  // useEffect(() => {
+  //   const getImage = async () => {
+  //     try {
+  //       const response = await axios.get(baseUrl);
+  //       setImage(response.data);
+  //       console.log("Image set", response.data.alt_description);
+  //     } catch (error) {
+  //       console.log("ERROR: ", error);
+  //     }
+  //   };
 
-    if (!image) {
-      getImage();
-    }
-  }, [image]);
+  //   if (!image) {
+  //     getImage();
+  //   }
+  // }, [image]);
 
   return (
-    <div>
-      {image && <img src={image.urls.small} alt={image.alt_description} />}
+    <div className="container">
+      {/* {image && <img src={image.urls.small} alt={image.alt_description} />} */}
+      {
+        <img
+          src={
+            "https://images.unsplash.com/photo-1580253784018-8b2c4a9034c2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2Mjk4M30"
+          }
+          alt="a female adult cat walking in fall leaves"
+        />
+      }
     </div>
   );
 }
