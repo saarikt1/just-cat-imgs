3d2c2025381d7c5fa3d3e2bdd2c95e34865b9479
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-09-04 13:48:10 +0200

    Show cat image in the frontend

--- api/index.js
+++ api/index.js
@@ -2,10 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
+const morgan = require("morgan");
 global.fetch = fetch;
 
 const Unsplash = require("unsplash-js").default;
 const toJson = require("unsplash-js").toJson;
+
+const cors = require("cors");
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use((error, req, res, next) => {
+  console.log("Error status: ", error.status);
+  console.log("Message: ", error.message);
+
+  res.status(error.status || 500);
+
+  res.json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+  });
+});
 
 const unsplash = new Unsplash({
   accessKey: process.env.ACCESS_KEY,
@@ -17,6 +35,9 @@ app.get("/", (req, res) => {
     .then(toJson)
     .then((json) => {
       res.send(json);
+    })
+    .catch((err) => {
+      console.log("ERROR:", err);
     });
 });
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001";
 
 function App() {
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    const getImage = async () => {
+      try {
+        const response = await axios.get(baseUrl);
+        setImage(response.data);
+        console.log("Image set", response.data.alt_description);
+      } catch (error) {
+        console.log("ERROR: ", error);
+      }
+    };
+
+    if (!image) {
+      getImage();
+    }
+  }, [image]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Just Cat Imgs</p>
-      </header>
+    <div>
+      {image && <img src={image.urls.small} alt={image.alt_description} />}
     </div>
   );
 }
